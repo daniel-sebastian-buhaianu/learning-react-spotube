ca947f0cc3f39d18d3feabe0433a1f0c79f67998
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-21 08:12:36 +0300

    added event handlers for search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import { useState, useEffect } from 'react';
+
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
+
+  // Not working now, need to find a way around CORS
+  const searchYouTube = text => {
+    try {
+    fetch(`https://www.youtube.com/results?search_query=${text}`)
+      .then(response => console.log(response))
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  
+  const handleKeyDown = event => {
+    if (event.code === "Enter") {
+      searchYouTube(event.target.value);
+    }
+  };
+  
+  const handleOnClick = event => {
+    searchYouTube(event.currentTarget.previousElementSibling.value);
+  };
+
   return (
     <div className='App'>
       <div className='container'>
         <h1 className='app-title'>SpoTube</h1>
-        <SearchBox placeholder={'Search anything from YouTube...'}/>
+        <SearchBox 
+          placeholder={'Search anything from YouTube...'}
+          handleKeyDown={handleKeyDown}
+          handleOnClick={handleOnClick}
+        />
       </div>
     </div>
   );
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,14 +1,15 @@
 import './search-box.styles.css';
 import SearchIcon from '@mui/icons-material/Search';
 
-const SearchBox = ({ className, placeholder }) => {
+const SearchBox = ({ className, placeholder, handleKeyDown, handleOnClick }) => {
     return (
         <div className={`search-box ${className}`}>
             <input
                 type='search'
                 placeholder={placeholder}
+                onKeyDown={handleKeyDown}
             />
-            <button className='search-btn'>
+            <button className='search-btn' onClick={handleOnClick}>
                 <SearchIcon className='search-icon'/>
             </button>
         </div>
