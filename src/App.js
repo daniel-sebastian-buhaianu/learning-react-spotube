44441a5e222c3dcd0061d71b12f6cece9264d210
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-21 06:47:36 +0300

    added media queries for responsiveness

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 const App = () => {
   return (
     <div className='App'>
-      <h1 className='app-title'>SpoTube</h1>
-      <SearchBox placeholder={'Search anything from YouTube...'}/>
+      <div className='container'>
+        <h1 className='app-title'>SpoTube</h1>
+        <SearchBox placeholder={'Search anything from YouTube...'}/>
+      </div>
     </div>
   );
 }
